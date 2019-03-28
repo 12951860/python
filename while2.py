d93952f7cb2ae2f327674433a1b974174af228e3
12951860 <skbm653@gmail.com>
2019-03-29 03:14:16 +0900

    20190328while

---
+++ while2.py
@@ -0,0 +1,214 @@
+Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 23:09:28) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=0
+>>> while a < 10
+SyntaxError: invalid syntax
+>>> while a < 10:
+	a = a + 1
+	if a % 2 == 0
+	
+SyntaxError: invalid syntax
+>>> while a < 10:
+	a = a+1
+	if a % 2 == 0:
+		continue
+	print(a)
+
+	
+1
+3
+5
+7
+9
+>>> //continue는 while문의 처음으로 돌아가는 명령어
+SyntaxError: invalid syntax
+>>> while True:
+	print("Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.")
+
+	
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.
+Traceback (most recent call last):
+  File "<pyshell#14>", line 2, in <module>
+    print("Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.")
+KeyboardInterrupt
+>>> 
